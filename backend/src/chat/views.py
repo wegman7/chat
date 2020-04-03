02534c5f3a2b1468e36f3f093dcb1bfe92da32e9
Josh Wegman <josh@cherokee.local>
2020-04-02 21:53:04 -0400

    got basic chat app working with entirely django

---
+++ backend/src/chat/views.py
@@ -0,0 +1,12 @@
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render
+
+def index(request):
+    return render(request, 'chat/index.html', {})
+
+@login_required
+def room(request, room_name):
+    return render(request, 'chat/room.html', {
+        'room_name': room_name,
+        'username': request.user.username
+    })
